e30fb9da39685d4e74be0fc55f4bc40b3c292a6f
Shoaib <main.shoby@gmail.com>
2020-08-18 22:35:59 -0400

    produced basic components and wireframe layout.
    added home, services, about and contract components. added supporting libraries: bootstrap, various react dependencies.

---
+++ enterprice-services-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Services from './components/Services';
+import Contact from './components/Contact';
+import About from './components/About';
+
+function App() {
+  return (
+    <>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/services" component={Services} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/about" component={About} />
+      </Switch>
+    </>
+  );
+}
+
+export default App;
