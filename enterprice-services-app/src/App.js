3cf4c91114eae3a1fc840bff329f857973348f67
Shoaib Sheikh <main.shoby@gmail.com>
2020-08-18 22:46:46 -0400

    added projects wireframe component

--- enterprice-services-app/src/App.js
+++ enterprice-services-app/src/App.js
@@ -5,12 +5,14 @@ import Home from './components/Home';
 import Services from './components/Services';
 import Contact from './components/Contact';
 import About from './components/About';
+import Projects from './components/Projects'
 
 function App() {
   return (
     <>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/projects" component={Projects} />
         <Route exact path="/services" component={Services} />
         <Route exact path="/contact" component={Contact} />
         <Route exact path="/about" component={About} />
