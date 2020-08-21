82e6e23a46c0f5b038111b413ed1f511d65dd8c2
Shoaib Sheikh <ssheikh@kr.af.mil>
2020-08-21 12:58:50 -0400

    home page partially done, adds llama image and other css elements

--- enterprice-services-app/src/components/Home.js
+++ enterprice-services-app/src/components/Home.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import web from "../../src/images/Llama-face/SVG/open-glasses.svg"
 
 const Home = () => {
     return (<>
-        <h1>
-            This is Home page.
-    </h1>
+        <section id="header" className="">
+            <div className="container-fluid nav_bg">
+                <div className='row'>
+                    <div className="col-10 mx-auto">
+                        <div className="row">
+                            <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1">
+                                <h1 className="my-lg-5">
+                                    Home is where the <strong style={{color: '#ff707a'}}> heart </strong> is...
+                                    and also from where I <strong style={{color:'#ff707a'}}>work</strong> from.
+                                </h1>
+                                <h4 className="my-lg-5" >...<strong> Shoaib </strong>(Team Jigsaw)</h4>
+                                <div className="my-lg-5">
+                                    <a href="" className="my-3 btn btn-outline-info"> Learn More </a>
+                                </div>
+                                </div>
+                                <div className="col-lg-6 order-1 order-lg-2 header-img">
+                                    <img src={web} className="figure-img img-fluid rounded" alt="home img"/>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
     </>)
 }
 
--- enterprice-services-app/src/components/Navbar.jsx
+++ enterprice-services-app/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         <div className='row'>
           <div className="col-10 mx-auto">
       <nav className="navbar navbar-light navbar-expand-lg navbar-light">
-        <NavLink className="navbar-brand" to="/">JambaTheMonster</NavLink>
+        <NavLink exact className="navbar-brand" to="/">LlamaTheNerd</NavLink>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           {/*<span className="navbar-toggler-icon"></span>*/}
@@ -22,16 +22,16 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <NavLink className="nav-link" to={projects}>Projects</NavLink>
+              <NavLink exact activeClassName="menu_active" className="nav-link" to={projects}>Projects</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to={services}>Services</NavLink>
+              <NavLink exact activeClassName="menu_active" className="nav-link" to={services}>Services</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to={contact}>Contact</NavLink>
+              <NavLink exact activeClassName="menu_active" className="nav-link" to={contact}>Contact</NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to={about}>About</NavLink>
+              <NavLink exact activeClassName="menu_active" className="nav-link" to={about}>About</NavLink>
             </li>
             {/*<li className="nav-item dropdown">*/}
             {/*  <a className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-toggle="dropdown"*/}
