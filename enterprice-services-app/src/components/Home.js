addfcb8f2c00c8bec799357651af40ec15c4564d
Shoaib Sheikh <main.shoby@gmail.com>
2020-08-26 21:18:10 -0400

    tweaked home page refactor and links

--- enterprice-services-app/src/components/Home.js
+++ enterprice-services-app/src/components/Home.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import web from "../../src/images/Llama-face/SVG/open-glasses.svg"
+import {NavLink} from 'react-router-dom';
 
 const Home = () => {
     return (<>
-        <section id="header" className="">
+        <section id="header" className="d-flex align-items-center">
             <div className="container-fluid nav_bg">
                 <div className='row'>
                     <div className="col-10 mx-auto">
                         <div className="row">
-                            <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1">
-                                <h1 className="my-lg-5">
+                            <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1  d-flex justify-content-center flex-column">
+                                <h1 className="my-lg-5 ">
                                     Home is where the <strong style={{color: '#ff707a'}}> heart </strong> is...
-                                    and also from where I <strong style={{color:'#ff707a'}}>work</strong> from.
                                 </h1>
-                                <h4 className="my-lg-5" >...<strong> Shoaib </strong>(Team Jigsaw)</h4>
+                                <h4 className="my-lg-5" ><strong> Shoaib </strong>(Team Jigsaw)</h4>
                                 <div className="my-lg-5">
-                                    <a href="" className="my-3 btn btn-outline-info"> Learn More </a>
+                                    <NavLink to="/services" className="my-3 btn btn-outline-info"> Learn More </NavLink>
                                 </div>
                                 </div>
                             <div className="container">
                                 <div className="avatar">
-                                    <a href="https://codepen.io/MarioDesigns/">
+                                    <NavLink to="/about">
                                         <img src={web} alt="home img" className="navbar-nav ml-auto"/>
-                                    </a>
+                                    </NavLink>
                                 </div>
                             </div>
                         </div>
